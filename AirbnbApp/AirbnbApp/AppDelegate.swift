dc0538eb007caf7e3429d2c550d8e06cc7d1e7a8
HongzCloud <jihong21511@gmail.com>
2021-05-18 16:32:31 +0900

    Feat: Create project

---
+++ AirbnbApp/AirbnbApp/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  AirbnbApp
+//
+//  Created by 오킹 on 2021/05/18.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
---
+++ AirbnbApp/AirbnbApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirbnbApp
+//
+//  Created by 오킹 on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
